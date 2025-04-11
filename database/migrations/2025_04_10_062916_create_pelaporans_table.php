04dd5102b3f0965d22983eebbb89afe043600785
ikhlasrama16 <ikhlasrama16@gmail.com>
2025-04-11 11:08:05 +0700

    tabel & seed kategori pelanggaran user seed update

--- database/migrations/2025_04_10_062916_create_pelaporans_table.php
+++ database/migrations/2025_04_10_062916_create_pelaporans_table.php
@@ -13,41 +13,41 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pelaporans', function (Blueprint $table) {
-            $table->id();
+        // Schema::create('pelaporans', function (Blueprint $table) {
+        //     $table->id();
 
-            // Kolom ID Laporan (untuk ID custom seperti LAP00001)
-            $table->string('id_laporan')->unique();
+        //     // Kolom ID Laporan (untuk ID custom seperti LAP00001)
+        //     $table->string('id_laporan')->unique();
 
-            // Identitas Pelapor
-            $table->string('nama_pelapor');
-            $table->string('nik')->nullable();
-            $table->string('alamat')->nullable();
-            $table->string('no_telp')->nullable();
-            $table->string('email')->nullable();
-            $table->string('hubungan_dengan_terlapor')->nullable();
-            $table->enum('identitas_terbuka', ['ya', 'tidak'])->default('tidak');
+        //     // Identitas Pelapor
+        //     $table->string('nama_pelapor');
+        //     $table->string('nik')->nullable();
+        //     $table->string('alamat')->nullable();
+        //     $table->string('no_telp')->nullable();
+        //     $table->string('email')->nullable();
+        //     $table->string('hubungan_dengan_terlapor')->nullable();
+        //     $table->enum('identitas_terbuka', ['ya', 'tidak'])->default('tidak');
 
-            // Isi Pelaporan
-            $table->string('nama_terlapor')->nullable();
-            $table->string('jabatan_terlapor')->nullable();
-            $table->text('penjelasan')->nullable();
-            $table->text('lokasi_kejadian')->nullable();
-            $table->text('tanggal_kejadian')->nullable();
-            $table->text('file_bukti')->nullable();
+        //     // Isi Pelaporan
+        //     $table->string('nama_terlapor')->nullable();
+        //     $table->string('jabatan_terlapor')->nullable();
+        //     $table->text('penjelasan')->nullable();
+        //     $table->text('lokasi_kejadian')->nullable();
+        //     $table->text('tanggal_kejadian')->nullable();
+        //     $table->text('file_bukti')->nullable();
 
-            // Dugaan Orang Lain Terlibat
-            $table->json('dugaan_terlibat')->nullable();
+        //     // Dugaan Orang Lain Terlibat
+        //     $table->json('dugaan_terlibat')->nullable();
 
-            // Saksi Mata
-            $table->json('saksi_mata')->nullable();
+        //     // Saksi Mata
+        //     $table->json('saksi_mata')->nullable();
 
-            // Pertanyaan Tambahan
-            $table->text('kejadian_terulang')->nullable();
-            $table->string('kerugian_finansial')->nullable();
+        //     // Pertanyaan Tambahan
+        //     $table->text('kejadian_terulang')->nullable();
+        //     $table->string('kerugian_finansial')->nullable();
 
-            $table->timestamps();
-        });
+        //     $table->timestamps();
+        // });
     }
 
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // TABEL USER
+        DB::table('users')->delete();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'email@admin.com',
+                'password' => Hash::make('admin123456@&')
+            ]
+        ];
+
+        DB::table('users')->insert($users);
+
+        // TABEL KATEGORI PELANGGARAN
+        DB::table('kategori_pelanggarans')->delete();
+
+        $kategoripelanggaran = [
+            ['nama_kategori' => 'Pencurian atau penyalahgunaan Dana Bank atau dana Nasabah atau pihak lainnya'],
+            ['nama_kategori' => 'Pencurian atau penyalahgunaan non-cash asset (Informasi, database, inventaris kantor)'],
+            ['nama_kategori' => 'Pemalsuan (Dokumen, tandatangan)'],
+            ['nama_kategori' => 'Penyuapan/Gratifikasi'],
+            ['nama_kategori' => 'Pemerasan'],
+            ['nama_kategori' => 'Penerimaan tidak sah'],
+            ['nama_kategori' => 'Pelanggaran kode etik'],
+            ['nama_kategori' => 'Ketidaknyamanan kerja (perundungan, intimidasi, pelecehan, tindakan asusila lainnya)'],
+            ['nama_kategori' => 'Misselling/misleading penjualan produk'],
+            ['nama_kategori' => 'Pencucian uang dan pendanaan terorisme'],
+            ['nama_kategori' => 'Benturan kepentingan'],
+            ['nama_kategori' => 'Kecurangan laporan keuangan'],
+            ['nama_kategori' => 'Penipuan'],
+            ['nama_kategori' => 'Pembocoran informasi rahasia Bank'],
+        ];
+
+        DB::table('kategori_pelanggarans')->insert($kategoripelanggaran);
     }
 }
